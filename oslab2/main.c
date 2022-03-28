62c78f8c264289f6e7a9858adb92778a7baff003
Luka Sever <severluka045@gmail.com>
2022-03-28 21:31:30 +0200

    more progress backup

--- oslab2/main.c
+++ oslab2/main.c
@@ -9,12 +9,12 @@
  *  Isto vrijedi za radnu/izlaznu dretvu.
  *  Ulazna i radna dretva komuniciraju preko zajednicke memorije koju treba stvoriti na pocetku
  *  programa.
- *   
+ *
  * ===== exit codes and their causes =====
  * ----- code 3: shmget call returned -1, could not create memory segment -----
  */
 
-//TODO neki sigaction postaviti?
+// TODO neki sigaction postaviti?
 #include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
@@ -27,37 +27,49 @@
 int seg_id;
 int *shared_variable;
 
-void thread_in() {
+void *thread_in(void *x) {
+    printf("thread primio %d\n", *(int *)x);
     int sleep_for = rand() % 5 + 1;
     for (int i = 0; i < sleep_for; i++) {
         sleep(1);
     }  // TODO ovo se salje preko zajednicke memorije, shm funkcije
     *shared_variable = rand() % 100 + 1;
+    printf("Ulazna dretva: broj %d\n", *shared_variable);
 }
 
-int main(void) {
-    srand(time(NULL));
-    
-    seg_id = shmget(IPC_PRIVATE, sizeof(int), 0600);
-    if (seg_id == -1) exit(3);
-    shared_variable = (int *) shmat(seg_id, NULL, 0);
-    
-    thread_in();
-
-    if (fork() == 0) { // sad smo u drugom procesu, drugoj dretvi (radnoj - ona obraduje broj)
-        int pvar = *shared_variable;
-        pvar++;
-        
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        printf("At least one argument required!\n");
         exit(0);
     }
+    printf("Pokrenut glavni proces s ulaznom dretvom\n");
+    srand(time(NULL));
 
-    // int shmget(key_t key, size_t size, int flags);
+    seg_id = shmget(IPC_PRIVATE, sizeof(int), 0600);
+    if (seg_id == -1) exit(1);
+    shared_variable = (int *)shmat(seg_id, NULL, 0);
 
-    // UlaznaDretva operacije
+    // TODO saljes nesto u thread!
+
+    int n = atoi(argv[1]);
+    pthread_t thr_id;
+    pthread_create(&thr_id, NULL, thread_in, &n);
+    pthread_join(thr_id, NULL);
+
+    if (fork() == 0) {  // sad smo u drugom procesu, drugoj dretvi (radnoj - ona obraduje broj)
+        printf("Pokrenut proces s radnom i izlaznom dretvom (treuntno samo radna postoji)\n");
+        int pvar;
+        while ((pvar = *shared_variable) == 0)
+            ;
+        pvar++;
+        printf("Radna dretva: pročitan broj %d i povećan na %d\n", pvar - 1, pvar);
+        exit(0);
+    }
 
     exit(0);
     if (fork == 0) {
     }
-
+    exit(1);
+    // pthread_join
     return 0;
 }
