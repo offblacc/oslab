c7c8a0302b64d35d3c4587edc002dc5dc55f4d5b
Luka Sever <severluka045@gmail.com>
2022-03-28 19:10:09 +0200

    lab2 progress backup

--- oslab2/main.c
+++ oslab2/main.c
@@ -1,23 +1,62 @@
-/*Studenti čija je zadnja znamenka JMBAG-a neparna trebaju napisati program u kojem jedan od dva
-procesa ima jednu dretvu i to ulaznu, a drugi proces dvije dretve: radnu i izlaznu.*/
-/* Proces s ulaznom dretvom - svakih 1 do 5 sekundi generira nasumican broj, 1 do 100
-    i upise ga u zajednicku varijablu koju dijeli s drugim procesom u kojem je radna
-    dretva. Radna dretva cita taj broj, povecava ga za 1 i upisuje u drugu zajednicku
-    varijablu s izlaznom dretvom. Izlazna dretva procita taj broj i zapisuje ga u 
-    datoteku.
-    Broj nasumicnih brojeva koje ulazna dretva generira zadaje se kao argument pri pozivanju
-    glavnog programa.
-    Radna dretva čeka kada je u zajedničkoj varijabli s ulaznom dretvom 0.
-    Isto vrijedi za radnu/izlaznu dretvu.
-    Ulazna i radna dretva komuniciraju preko zajednicke memorije koju treba stvoriti na pocetku
-    programa.    
-    */
+/*  Proces s ulaznom dretvom - svakih 1 do 5 sekundi generira nasumican broj, 1 do 100
+ *  i upise ga u zajednicku varijablu koju dijeli s drugim procesom u kojem je radna
+ *  dretva. Radna dretva cita taj broj, povecava ga za 1 i upisuje u drugu zajednicku
+ *  varijablu s izlaznom dretvom. Izlazna dretva procita taj broj i zapisuje ga u
+ *  datoteku.
+ *  Broj nasumicnih brojeva koje ulazna dretva generira zadaje se kao argument pri pozivanju
+ *  glavnog programa.
+ *  Radna dretva čeka kada je u zajedničkoj varijabli s ulaznom dretvom 0.
+ *  Isto vrijedi za radnu/izlaznu dretvu.
+ *  Ulazna i radna dretva komuniciraju preko zajednicke memorije koju treba stvoriti na pocetku
+ *  programa.
+ *   
+ * ===== exit codes and their causes =====
+ * ----- code 3: shmget call returned -1, could not create memory segment -----
+ */
+
+//TODO neki sigaction postaviti?
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int seg_id;
+int *shared_variable;
+
+void thread_in() {
+    int sleep_for = rand() % 5 + 1;
+    for (int i = 0; i < sleep_for; i++) {
+        sleep(1);
+    }  // TODO ovo se salje preko zajednicke memorije, shm funkcije
+    *shared_variable = rand() % 100 + 1;
+}
 
 int main(void) {
-    int broj = 0; // nebu tak islo, trebal bu ti jedan typedef key_t int; int shmget(key_t key, int size, int flags) ;
+    srand(time(NULL));
+    
+    seg_id = shmget(IPC_PRIVATE, sizeof(int), 0600);
+    if (seg_id == -1) exit(3);
+    shared_variable = (int *) shmat(seg_id, NULL, 0);
+    
+    thread_in();
 
-    if (fork() == 0) {
-        // sto radi proces dijete - proces s 2 dretve
+    if (fork() == 0) { // sad smo u drugom procesu, drugoj dretvi (radnoj - ona obraduje broj)
+        int pvar = *shared_variable;
+        pvar++;
+        
+        exit(0);
+    }
+
+    // int shmget(key_t key, size_t size, int flags);
+
+    // UlaznaDretva operacije
+
+    exit(0);
+    if (fork == 0) {
     }
 
     return 0;
