9d05eb948f1768eb5205ba84636934d36ded457c
Luka Sever <severluka045@gmail.com>
2022-04-28 21:52:55 +0200

    radi jedno pokretanje, ukrcavanje i vrtnja

--- oslab3/vrtuljak.c
+++ oslab3/vrtuljak.c
@@ -4,21 +4,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+int terminate_threads = 0;
+int seats_num, visitors_num;
+sem_t seats_sem, start_carousel_sem;
 
 void process_sigint(int sig) {
     exit(1);
 }
+
 void *thread_visitor(void *x) {
-    printf("Thread visitor started - not functional yet\n");
+    printf("Thread visitor started\n");
+    sem_wait(&seats_sem);
+    sem_post(&start_carousel_sem);
+    printf("Boarded the carousel.\n");
 }
 
 void *thread_carousel(void *x) {
-    printf("Thread carousel started - not functional yet\n");
-}
+    while (true) {
+        printf("Thread carousel started\n");
+        for (int i = 0; i < seats_num; i++) {
+            sem_wait(&start_carousel_sem);
+        }
+        printf("Carousel is ready.\n");
+        for (int i = 1; i <= 5; i++) {
+            printf("Carousel is spinning (%d).\n", i);
+        }
 
-int terminate_threads = 0;
-int carousel_seats_num, visitors_num;
-sem_t *seats_sem;
+    }
+}
 
 int main(int argc, char **argv) {
     // process sigint signal
@@ -28,28 +43,28 @@ int main(int argc, char **argv) {
     sigaddset(&act.sa_mask, SIGTERM);
     act.sa_flags = 0;
     sigaction(SIGINT, &act, NULL);
-
     // ---- check if input is valid ----
     if (argc != 3) {
         printf("Two (2) arguments required, number of seats on the carousel and number of visitors (in that order)\n");
         exit(0);
     }
 
-    carousel_seats_num = (int)strtol(argv[1], &argv[1], 10);
+    seats_num = (int)strtol(argv[1], &argv[1], 10);
     visitors_num = (int)strtol(argv[2], &argv[2], 10);
-    if (carousel_seats_num < 1 || visitors_num < 1) {
+    if (seats_num < 1 || visitors_num < 1) {
         printf("Invalid number of seats or visitors.\n");
         exit(0);
     }
 
-    if ((carousel_seats_num > visitors_num)) {
+    if ((seats_num > visitors_num)) {
         printf("Carousel has more seats than there are visitors, it will never start.\n");
         exit(0);
     }
 
-    pthread_t thr_id[argc];  // create array of thread ids
-    seats_sem = malloc(sizeof(sem_t) * carousel_seats_num);
-    sem_init(seats_sem, 0, (unsigned)carousel_seats_num);  // init semaphore to number of seats on the carousel
+    pthread_t thr_id[argc];              // create array of thread ids
+    sem_init(&seats_sem, 0, seats_num);  // init semaphore to number of seats on the carousel
+
+    sem_init(&start_carousel_sem, 0, 0);  // this will be awaited by the carousel, signalizing him to check if he can start
 
     // create all threads
     pthread_create(&thr_id[0], NULL, thread_carousel, NULL);
@@ -57,7 +72,7 @@ int main(int argc, char **argv) {
         pthread_create(&thr_id[i], NULL, thread_visitor, &i);
     }
 
-    // cekaj da se sve dretve završe
+    // wait for all threads to finish
     for (int i = 0; i < visitors_num + 1; i++) {
         pthread_join(thr_id[i], NULL);
     }
